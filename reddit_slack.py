984965f7ac7e669043f99ba57812f27bbcedd86d
Carter Halamka <chalamka@gmail.com>
2016-05-11 13:13:08 -0500

    Finish slack notification function

--- reddit_slack.py
+++ reddit_slack.py
@@ -59,11 +59,16 @@ class BattleNotifier:
             return []
 
     def _simul_check(self, battle):
-        pass
+        simuls = []
+
+        for b in self.battles:
+            if abs(b.time - battle.time) < 5 and battle.battle_id != b.battle_id:
+                simuls.append(b)
+
+        return simuls
 
     def _slack_notification(self):
         attachments = []
-        payload = None
         thumb_url = "http://na.wargaming.net/clans/media/clans/emblems/cl_{}/{}/emblem_64x64.png"
         current_time = dt.datetime.now()
 
@@ -76,19 +81,23 @@ class BattleNotifier:
                 minutes_until_battle = time_until_battle.total_seconds() / 60
 
                 if battle.type == 'attack' and battle.attack_type == 'tournament':
-                    time_text = "Tournament Round {} of {} begins at {} CST popping in {} minutes".format(
+                    time_text = "Tournament Round {} of {} begins at {} CST popping in ~{} minutes".format(
                         province.round_number,
                         ceil(log2(len(province.attackers))),
                         battle_start_time.strftime("%H:%M"),
-                        minutes_until_battle)
+                        minutes_until_battle - 14)
                 else:
-                    time_text = "{} begins at {} CST popping in {} minutes".format(
+                    time_text = "{} begins at {} CST popping in ~{} minutes".format(
                         battle.type.title(),
                         battle_start_time.strftime("%H:%M"),
-                        minutes_until_battle)
+                        minutes_until_battle - 14)
 
-                if self._simul_check(battle):
-                    simul_text = ""
+                simul_text = ""
+                simuls = self._simul_check(battle)
+                if simuls:
+                    simul_text = "This battle appears to occur simultaneously with {} other battles: {}.".format(
+                            len(simuls),
+                            ", ".join([b.province_name for b in simuls]))
 
                 battle_attachment = slack.build_slack_attachment(pretext="Upcoming CW battle vs. {}".format(clan.tag),
                                                                  fields=[],
@@ -96,7 +105,10 @@ class BattleNotifier:
                                                                  level="good" if battle.type == 'defence' else "danger",
                                                                  thumb_url=thumb_url.format(clan.clan_id[-3:], clan.clan_id),
                                                                  text="{}\n{}\n{}".format(province_text, time_text, simul_text))
-                attachments.append()
+                attachments.append(battle_attachment)
+
+        payload = slack.build_slack_payload(attachments, "", self.bot_name, self.icon_emoji, self.channel_name)
+        slack.send_slack_webhook(self.slack_url, payload)
 
     def _configure_logging(self, level):
         abspath = os.path.abspath(__file__)
@@ -134,164 +146,10 @@ def configure_parser():
     parser = argparse.ArgumentParser()
     subparser = parser.add_subparsers()
 
-    cw_parser = subparser.add_parser('cw_notification')
-    cw_parser.set_defaults(func=cw_notification)
-
-    sh_parser = subparser.add_parser('sh_notification')
-    sh_parser.set_defaults(func=sh_notification)
-
     daemon = subparser.add_parser('daemon')
     daemon.set_defaults(func=notify_loop)
 
     return parser
-
-
-def cw_notification(args):
-    cw_attachment = []
-    cw_messages = None
-
-    cw_api_response = wot.get_cw_battles(config['application_id'], config['clan_id'])
-    cw_battles = process_cw_battles(cw_api_response)
-
-    if cw_battles:
-        cw_messages = create_cw_battle_message(cw_battles)
-        cw_fields = [slack.build_slack_field(b['title'], b['message']) for b in cw_messages]
-        message_color = "danger" if cw_battles[0]['type'] == 'attack' else 'good'
-        cw_attachment = [slack.build_slack_attachment("Upcoming clanwars battle",
-                                                      "List of upcoming Clan Wars battles", "", message_color, cw_fields)]
-
-    if cw_messages:
-        cw_payload = slack.build_slack_payload(cw_attachment, "<!channel> Upcoming battles", config['bot_name'],
-                                               config['icon_emoji'], config['channel_name'])
-        slack.send_slack_webhook(config['slack_url'], cw_payload)
-
-
-def sh_notification(args):
-    sh_attachment = []
-
-    sh_api_response = wot.get_sh_battles(config['application_id'], config['clan_id'])
-    sh_battles = process_sh_battles(sh_api_response)
-
-    if sh_battles:
-        sh_messages = create_sh_battle_message(sh_battles)
-        sh_fields = [slack.build_slack_field(b['title'], b['message']) for b in sh_messages]
-        sh_attachment = [slack.build_slack_attachment("Upcoming stronghold battle",
-                                                      "List of upcoming Stronghold battles:", "", "#D00000", sh_fields)]
-
-    if sh_attachment:
-        sh_payload = slack.build_slack_payload(sh_attachment, "<!channel> Upcoming battles", config['bot_name'],
-                                               config['icon_emoji'], config['channel_name'])
-        slack.send_slack_webhook(config['slack_url'], sh_payload)
-
-
-def process_cw_battles(cw_battles):
-    if cw_battles['status'] != 'ok':
-        log.critical("wargaming globalmap/clanbattles API returned error status: {}".format(cw_battles['status']))
-        sys.exit(1)
-    else:
-        if cw_battles['meta']['count'] != 0:
-            log.info("Found {} cw battles to process".format(cw_battles['meta']['count']))
-            cw_battles = cw_battles['data']
-        else:
-            log.info("No cw battles to process")
-            cw_battles = []
-
-    return cw_battles
-
-
-def process_sh_battles(sh_battles):
-    if sh_battles['status'] != 'ok':
-        log.critical("wargaming stronghold/plannedbattles API returned error status: {}".format(sh_battles['status']))
-        sys.exit(1)
-    else:
-        if sh_battles['meta']['count'] != 0:
-            log.info("Found {} cw battles to process".format(sh_battles['meta']['count']))
-            sh_battles = sh_battles['data'][config['clan_id']]
-        else:
-            log.info("No sh battles to process")
-            sh_battles = []
-
-    return sh_battles
-
-
-def create_cw_battle_message(cw_battles):
-    cw_battles = [c for c in cw_battles]
-    cw_battles.sort(key=lambda x: x['time'])
-
-    current_time = dt.datetime.now()
-
-    cw_fields = []
-    processed = list(load_config('processed.json'))
-
-    if cw_battles:
-        for battle in cw_battles:
-            battle_time = dt.datetime.fromtimestamp(int(battle['time']))
-            battle_id = str(battle['time']) + battle['province_name']
-
-            if battle_time > current_time and battle_id not in processed:
-                cw_fields.append(format_cw_battle(battle))
-                processed.append(battle_id)
-
-    write_json('processed.json', processed)
-
-    return cw_fields
-
-
-def create_sh_battle_message(sh_battles):
-    sh_battles_list = sh_battles
-    sh_fields = []
-
-    if sh_battles_list:
-        for battle in sh_battles_list:
-            sh_fields.append(format_sh_battle(battle))
-
-    return sh_fields
-
-
-def format_cw_battle(battle):
-    battle_time = dt.datetime.fromtimestamp(int(battle['time']))
-    current_time = dt.datetime.now()
-    time_delta = battle_time - current_time
-    hours_to_battle = int((time_delta.seconds / 60) / 60)
-    minutes_to_battle = int((time_delta.seconds / 60) % 60)
-
-    competitor_clan_info = process_clan_info(wot.get_clan_info(config['application_id'], battle['competitor_id']),
-                                             battle['competitor_id'])
-
-    province_info = process_province_info(wot.get_province_info(config['application_id'],
-                                                                battle['front_id'],
-                                                                battle['province_id']))
-
-    text = ":rddt: RDDT vs {} :fire:\n" \
-           "Province: {} - Map: {}\n" \
-           "Battle starts in {} hour(s) and {} minute(s) [{} at {} CST]"\
-        .format(competitor_clan_info[str(battle['competitor_id'])]['tag'],
-                province_info[0]['province_name'],
-                province_info[0]['arena_name'],
-                hours_to_battle,
-                minutes_to_battle,
-                battle_time.strftime("%m/%d/%Y"),
-                battle_time.strftime("%H:%M"))
-
-    # wait 1 second between calls in order to stay below API restriction
-    time.sleep(1)
-    return {'title': ":siren: Clan Wars {} :siren:".format(battle['type']), 'message': text}
-
-
-def format_sh_battle(battle):
-    battle_time = dt.datetime.fromtimestamp(int(battle['battle_planned_date']))
-    current_time = dt.datetime.now()
-    time_delta = battle_time - current_time
-    hours_to_battle = int((time_delta.seconds / 60) / 60)
-    minutes_to_battle = int((time_delta.seconds / 60) % 60)
-    message = ":rddt: {} vs. {} :fire:\nSpecial pops in {} hour(s) and {} minute(s) [{} at {} CST]" \
-        .format(battle['attacker_clan_tag'],
-                battle['defender_clan_tag'],
-                hours_to_battle,
-                minutes_to_battle,
-                battle_time.strftime("%m/%d/%Y"),
-                battle_time.strftime("%H:%M"))
-    return {'title': ":siren: Stronghold {} :siren:".format(battle['battle_type']), 'message': message}
 
 
 def main(args):
